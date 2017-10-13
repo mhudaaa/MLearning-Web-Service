7cda19107f41d94a9478af5de25e061fc6a86c6d
mhudaaa <heihudaa@gmail.com>
2017-10-14 01:42:29 +0700

    test edit: coba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mobile Learning UNEJ
+                    Coba
                 </div>
 
                 <div class="links">
