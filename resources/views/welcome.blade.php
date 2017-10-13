30cd548f9d01b5520b0711260e3b26ad721dbe36
mhudaaa <heihudaa@gmail.com>
2017-10-14 01:34:33 +0700

    test edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mobile Learning
+                    UNEJ Mobile Learning
                 </div>
 
                 <div class="links">
